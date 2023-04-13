26347f1e68b3bfd9d0f98107beaf301fcc24de62
Trung <letrung041201@gmail.com>
2023-04-14 01:23:35 +0700

    hoan thanh CRUD voi node va seque

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,9 +4,7 @@ import CRUDService from "../services/CRUDService";
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
-        console.log('------------------------')
-        console.log(data)
-        console.log('------------------------')
+
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
@@ -27,13 +25,48 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('----------------------------------------');
-    console.log(data);
-    console.log('----------------------------------------');
 
     return res.render('displayCRUD.ejs', {
         dataTable: data
     });
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    
+    if(userId){
+        let userData = await CRUDService.getUserInfoById(userId);
+        // check user data not found
+
+        // let userData
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    }
+    else {
+        return res.send('user not found');
+    }
+    
+ 
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    });
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if(id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete User Succeed');
+    }else {
+        return res.send('User not found');
+    }
+
 }
 
 module.exports = {
@@ -41,4 +74,7 @@ module.exports = {
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD
 }
